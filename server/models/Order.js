612cf60aa80bb12472e341f92733e3059e0165ac
Gromit <eversoryan@gmail.com>
2022-09-22 19:21:06 -0600

    Creating model for Order and category

---
+++ server/models/Order.js
@@ -0,0 +1,18 @@
+const { Schema } = require('mongoose');
+
+const orderSchema = new Schema({
+    purchaseDate: {
+        type: Date,
+        default: Date.now
+    },
+    listing: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Listing'
+        }
+    ]
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
