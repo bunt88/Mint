b149f1ffd6dc61c768ec4d9cf91b230c85e5d22f
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-26 22:31:47 -0600

    added starting pieces for LoginSignup.js and fixed some bugs in the Login and SignUp components

--- mint/client/src/components/login.js
+++ mint/client/src/components/login.js
@@ -4,7 +4,6 @@ function Login(props) {
 
   return (
     <React.Fragment>
-      <div className="d-flex vh-100 w-100 justify-content-center" style={{backgroundColor: "rgb(204, 221, 224)"}}>
         <div className="d-flex m-5 flex-column align-items-center border border-dark rounded" style={{height: "275px", width: "250px", backgroundColor: "rgb(248, 247, 245)"}}>
             <div className="w-100 text-center p-3 font-weight-bold" style={{backgroundColor: "lightgreen"}}>Login</div>
             <div className="w-75">
@@ -15,18 +14,21 @@ function Login(props) {
                     <input class="w-100" type="text" name="password" placeholder="Password"></input>
                 </div>
                 <div className="m-3">
-                    <button className="w-100">Sign In</button>
+                    <button className="w-100" onClick={submit}>Sign In</button>
                 </div>
             </div>
             <div>
                 <div className="text-center font-weight-light">Don't have an account?</div>
-                <p className="text-center" onClick={() => props.setPage('signup')}>Sign Up</p>
+                <p className="text-center" onClick={() => props.setPage("signUp")}>Sign Up</p>
             </div>
-        </div>
         </div>
 
     </React.Fragment>
   );
+
+  function submit() {
+    console.log("test sign in button")
+  }
 }
 
 export default Login;
--- mint/client/src/components/sign_up.js
+++ mint/client/src/components/sign_up.js
@@ -3,7 +3,6 @@ import React from "react";
 function SignUp(props) {
   return (
     <React.Fragment>
-        <div className="d-flex vh-100 w-100 justify-content-center" style={{backgroundColor: "rgb(204, 221, 224)"}}>
             <div className="d-flex m-5 flex-column align-items-center border border-dark rounded" style={{height: "300px", width: "250px", backgroundColor: "rgb(248, 247, 245)"}}>
                 <div className="w-100 text-center p-3 font-weight-bold" style={{backgroundColor: "lightgreen"}}>Sign Up</div>
                 <div className="w-75">
@@ -17,16 +16,19 @@ function SignUp(props) {
                         <input className="w-100" type="text" name="password" placeholder="Password"></input>
                     </div>
                     <div className="m-3">
-                        <button className="w-100">Sign Up</button>
+                        <button className="w-100" onClick={submit}>Sign Up</button>
+                        <button className="w-100" onClick={() => props.setPage('login')}>Back to Login</button>
                     </div>
                 </div>
                 <div>
                     <div className="text-center">Welcome to Mint!</div>
                 </div>
             </div>
-            </div>
     </React.Fragment>
   );
+  function submit() {
+    console.log("test submit button")
+  }
 }
 
 export default SignUp;
