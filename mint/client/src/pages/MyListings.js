a1b6a09bf554bf217ca6112d84a381c1176808c0
unknown <bunt88@gmail.com>
2022-09-27 19:20:36 -0600

    Moved home and mylisting to pages and deleted unneeded code

--- mint/client/src/pages/MyListings.js
+++ mint/client/src/pages/MyListings.js
@@ -1,11 +1,22 @@
 import React from "react";
-import Naviagation from "../components/Navbar";
+// import Naviagation from "../components/Navbar";
 
 const MyListing = () => {
   return (
-    <div>
-      <Naviagation></Naviagation>
-    </div>
+    <>
+      <div className="container">
+        <div className="card" style="width: 18rem;">
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card Title</h5>
+            <p className="card-text">...</p>
+            <a href="#" className="btn btn-primary">
+              ...
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
