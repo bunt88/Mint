e96bb330c4489d2a377eddb6611e4b37cf50e246
Taryn Moore <tarynmoore628@gmail.com>
2022-09-27 20:51:27 -0600

    fixed bugs so we got no errors.

--- mint/client/src/pages/Home.js
+++ mint/client/src/pages/Home.js
@@ -4,12 +4,11 @@ const Home = () => {
   return (
     <div className="container">
       <div className="card" >
-        <img src="..." className="card-img-top" alt="..." />
+        <img src="" className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card Title</h5>
           <p className="card-text">...</p>
           <a href="#" className="btn btn-primary">
-            ...
           </a>
         </div>
       </div>
