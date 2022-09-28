8df813f891f2198094aa96f2ceabd6a57c17960f
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2022-09-27 21:30:12 -0600

    commented out mutations

--- mint/client/src/utils/mutations.js
+++ mint/client/src/utils/mutations.js
@@ -48,83 +48,83 @@ export const ADD_USER = gql`
     }
 `
 
-export const ADD_LISTING = gql`
-    mutation addListing(
-        $title: String!
-        $image: String!
-        $price: Float!
-        $description: String!
-        $category: String!
-    ) {
-        addListing(
-            title: $title
-            image: $image
-            price: $price
-            description: $description
-            category: $category
-        ) {
-            token
-            user {
-                _id
-            }
-        }
-    }
-`
+// export const ADD_LISTING = gql`
+//     mutation addListing(
+//         $title: String!
+//         $image: String!
+//         $price: Float!
+//         $description: String!
+//         $category: String!
+//     ) {
+//         addListing(
+//             title: $title
+//             image: $image
+//             price: $price
+//             description: $description
+//             category: $category
+//         ) {
+//             token
+//             user {
+//                 _id
+//             }
+//         }
+//     }
+// `
 
-export const UPDATE_USER = gql`
-    mutation updateUser(
-        $id: String!
-        user {
-            $username: String
-            $email: String
-            $wishList: [String]
-        }
-    ) {
-        token
-        user {
-            _id
-        }
-        username
-        email
-        wishList
-    }
-`
-export const UPDATE_LISTING = gql`
-    mutation updateListing(
-        $id: String!
-        listing: {
-            $title: String
-            $image: String
-            $price: Float
-            $description: String
-            $category: String
-        }
-    ) {
-        token
-        user {
-            _id
-        }
-        listing {
-            title
-            image
-            price
-            description
-            category
-        }
-    }
-`
+// export const UPDATE_USER = gql`
+//     mutation updateUser(
+//         $id: String!
+//         user {
+//             $username: String
+//             $email: String
+//             $wishList: [String]
+//         }
+//     ) {
+//         token
+//         user {
+//             _id
+//         }
+//         username
+//         email
+//         wishList
+//     }
+// `
+// // export const UPDATE_LISTING = gql`
+// //     mutation updateListing(
+// //         $id: String!
+// //         listing: {
+// //             $title: String
+// //             $image: String
+// //             $price: Float
+// //             $description: String
+// //             $category: String
+// //         }
+// //     ) {
+// //         token
+// //         user {
+// //             _id
+// //         }
+// //         listing {
+// //             title
+// //             image
+// //             price
+// //             description
+// //             category
+// //         }
+// //     }
+// // `
 
-export const REMOVE_LISTING = gql`
-    mutation removeListing(
-        $id: String!
-    ) {
-        removeListing(
-            where: { id: {_eq: $id} }
-        ) {
-            token
-            user {
-                _id
-            }
-        }
-    }
-`
+// export const REMOVE_LISTING = gql`
+//     mutation removeListing(
+//         $id: String!
+//     ) {
+//         removeListing(
+//             where: { id: {_eq: $id} }
+//         ) {
+//             token
+//             user {
+//                 _id
+//             }
+//         }
+//     }
+// `
