afe355be60e38cd542f4609b44ff46ba31c30435
Taryn Moore <tarynmoore628@gmail.com>
2022-09-29 20:34:43 -0600

    added the data to the singleAntique page.

--- mint/client/src/components/ListingCards/singleAntique.js
+++ mint/client/src/components/ListingCards/singleAntique.js
@@ -1,48 +1,48 @@
 import React from "react"
 
-// import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
-// // import Col from "react-bootstrap/Col";
-// import Button from "react-bootstrap/Button";
-// import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function SingleAntique({ antique }) {
   console.log(antique);
   // if (listings._id === categories._id) {
     // console.log(Antiques);
   return (
-    <div>Hello</div>
-  //   <div>
-  //     <Container
-  //       fluid
-  //       style={{ background: "rgb(204, 221, 224)" }}
-  //       className="text-center"
-  //     >
-  //       <Row className="justify-content-center">
-  //         <Card
-  //           className="m-3 pt-3 align-items-center"
-  //           style={{ width: "18rem", background: "rgb(248, 247, 245)" }}
-  //         >
-  //           <Card.Img
-  //             className="m-2"
-  //             variant="top"
-  //             // src={antiqueList.image}
-  //             style={{ width: 250, height: 150 }}
-  //           />
-  //           {/* {Antiques && Antiques.map((title, image, price, description) => ( */}
-  //           <Card.Body>
-  //             {/* <Card.Title key={title}> {title} </Card.Title> */}
-  //             <Card.Text>info on Antiques</Card.Text>
-  //             <Button className="" style={{ background: "rgb(204, 221, 224)" }}>
-  //               See Antiques
-  //             </Button>
+    <div>
+      <Container
+        fluid
+        style={{ background: "rgb(204, 221, 224)" }}
+        className="text-center"
+      >
+        <Row className="justify-content-center">
+          <Card
+            className="m-3 pt-3 align-items-center"
+            style={{ width: "18rem", background: "rgb(248, 247, 245)" }}
+          >
+            <Card.Img
+              className="m-2"
+              variant="top"
+              src={antique.image}
+              style={{ width: 250, height: 150 }}
+            />
+            {/* {Antiques && Antiques.map((title, image, price, description) => ( */}
+            <Card.Body>
+              <Card.Title> {antique.title} </Card.Title>
+              <Card.Text>{antique.price}</Card.Text>
+              <Card.Text>{antique.description}</Card.Text>
+              <Button className="" style={{ background: "rgb(204, 221, 224)" }}>
+                Add to Cart
+              </Button>
              
-  //           </Card.Body>
-  //           {/* ))}; */}
-  //         </Card>
-  //       </Row>
-  //     </Container>
-  //   </div>
+            </Card.Body>
+            {/* ))}; */}
+          </Card>
+        </Row>
+      </Container>
+    </div>
   )
   
 }
